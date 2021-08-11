6c99dad05b37570060410484a66d1e1cffaf3e2a
Dhruv Patel <dhruvpatel1862000b201@gmail.com>
2021-08-11 17:42:08 +0530

    validation added

--- update.php
+++ update.php
@@ -69,7 +69,7 @@ if(isset($_POST['Name'])){
                 echo '</div>';
                 echo '<h1>Category Name</h1>';
                 echo '<div class="input-box">';
-                    echo '<input type="text" name="Name" value="'.$row['Name'].'" id="name" onblur="validatename()"  placeholder="Category name"><br>';
+                    echo '<input type="text" name="Name" value="'.$row['Name'].'" id="name"   placeholder="Category name"><br>';
                     echo '<span id="err1"></span>';
                 echo '</div>';
                 echo '<br><hr>'
@@ -80,67 +80,20 @@ if(isset($_POST['Name'])){
             echo '</div>';
         echo '</form>';
         ?>
-        
-        <!--
+
         <footer>
-            <div class="main-content">>
-                <div class="left box">
-                    <h2>About Us</h2>
-                    <div class="content">
-                        <p>This is the place where you will get all the info. related to sports.</p>
-                        <div class="social">
-                            <a href="#"><span class="fab fa-facebook-f"></span></a>
-                            <a href="#"><span class="fab fa-twitter"></span></a>
-                            <a href="#"><span class="fab fa-instagram"></span></a>
-                            <a href="#"><span class="fab fa-youtube"></span></a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="center box">
-                    <h2>Contact Info</h2>
-                    <div class="content">
-                        <div class="place">
-                            <span class="fas fa-map-marker-alt"></span>
-                            <span class="text">Surat, Gujarat, India.</span>
-                        </div>
-                        <div class="phone">
-                            <span class="fas fa-phone-alt"></span>
-                            <span class="text">+91-9999999999</span>
-                        </div>
-                        <div class="email">
-                            <span class="fas fa-envelope"></span>
-                            <span class="text">abc@isportsinfo.com</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="right box">
-                    <h2>Reach US</h2>
-                    <div class="content">
-                        <form action="#">
-                            <div class="email">
-                                <div class="text">Email *</div>
-                                <input type="email" required>
-                            </div>
-                            <div class="msg">
-                                <div class="text">
-                                    Message *
-                                </div>
-                                <textarea name="name" rows="2" cols="20" required></textarea>
-                            </div>
-                            <div class="btn">
-                                <button type="submit">Send</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
             <div class="cpy">
-                <p>Created By @iSportsInfo |  ©2021 All Rights Reserved.</p>
+                <p>Created By @iSportsInfo |  ©2021 All Rights Reserved. | Follow on 
+                <a href="#"><span class="fab fa-facebook-f"></span></a>
+                <a href="#"><span class="fab fa-twitter"></span></a>
+                <a href="#"><span class="fab fa-instagram"></span></a>
+                <a href="#"><span class="fab fa-youtube"></span></a>
+                </p>
             </div>
-        </footer>-->
+        </footer>
     </div>
+
+    <script src="validate.js"></script>
 </body>
 </html>
 
--- validate.js
+++ validate.js
@@ -7,8 +7,16 @@ function validatename(){
     nameNode.style.border = "2px green solid";
     nameNode.style.backgroundColor = "cyan";
     let name = nameNode.value;
+    
+    let regexpress=new RegExp("^([a-zA-Z])*$");
     if(name===""){
         errNode1.innerHTML = "<b> This field is required.</b>";
+        nameNode.style.border = "2px red solid";
+        nameNode.style.backgroundColor = "pink";
+        return false;
+    }
+    else if(regexpress.test(name)==false){
+        errNode1.innerHTML = "<b> Enter Alphabets only...</b>";
         nameNode.style.border = "2px red solid";
         nameNode.style.backgroundColor = "pink";
         return false;
