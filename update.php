dfd6bfd046afb87f319d8b4902cca01b0c67252b
Dhruv Patel <dhruvpatel1862000b201@gmail.com>
2021-08-11 16:32:59 +0530

    Updation logic added...

--- update.php
+++ update.php
@@ -1,7 +1,5 @@
 <?php
 //INSERT INTO `category` (`s.no.`, `Name`) VALUES ('1', 'Cricket');
-
-
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -16,59 +14,33 @@ if (!$conn) {
 }
 
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $Name = $_POST["Name"];
 
-    $sql = "INSERT INTO `category` (`Name`) VALUES ('$Name')";
+$id = $_GET['id'];
+$sql = "SELECT `Name` FROM `category` WHERE `category`.`srno` = '$id'" ;
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+/*
+if(isset($_POST['Name'])){
+    $name = $_POST['Name'];
+    $sql = "UPDATE `category` SET  `Name` = '$name' WHERE `category`.`srno` = '$id'";
     $result = mysqli_query($conn, $sql);
-    
-
-    if($result){
-        echo "The record has been inserted successfully...<br>";
-        //$insert = true;
-    }
-    else{
-        echo "The record was not inserted successfully because of this error -->". mysqli_error($conn);
-    }
-    
-
-}
+}*/
 
 ?>
-
-
+<?/*php header("Location: index.php")*/?>
 
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-     
-    
-    <link rel="stylesheet" href="//cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">  
-    <script src="jquery-3.6.0.min.js"></script>  
-    <script src="//cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
-    
-
-    <script>
-        $(document).ready( function () {
-        $('#myTable').DataTable();
-        } );
-    </script>
-
-   
-
     <title>iSportsInfo</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="main2.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <script src="validate.js"></script>
 </head>
-
 <body>
     <div class="box-area">
         <header>
@@ -84,63 +56,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
                 </nav>
             </div>
         </header>
-
-       
-
         <div class="banner-area">
-            <h2>Manage Categories..</h2>
+            <h2>Add Categories..</h2>
         </div>
-
-       
-
-        <!--Applying CRUD-->
-        <div class="container my-4" >
-
-            <div id="crctg">
-                <a href="add_ctg.php"><button class='add btn btn-sm btn-primary'>Create Category</button></a>
-            </div>
-    
-    
-            <table class="table" id="myTable">
-                <thead>
-                    <tr>
-                        <th scope="col">srno</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $sql = "SELECT * FROM `category`";
-                    $result = mysqli_query($conn, $sql);
-                    $srno = 0;
-                    while($row = mysqli_fetch_assoc($result)){
-                        $srno = $srno + 1;
-                        echo "<tr>
-                        <th scope='row'>". $srno ."</th>
-                        <td>". $row['Name'] ."</td>
-                        <td> <a href='update.php?id=" . $row['srno'] . " '><button class='edit btn btn-sm btn-primary'>Edit</button></a> 
-                        <a href='delete.php?id=" . $row['srno'] . " '><button class='delete btn btn-sm btn-primary'>Delete</button></a> </td>
-                        
-                    </tr>";
-                       
-                    }
+        <?php
+        echo '<form  action="edit.php?id=" onsubmit="return validateform()" method="POST">';
+            echo '<div class="form-box">';
+                echo '<h1>Sr.No.</h1>';
+                echo '<div class="input-box">';
+                    echo '<input type="number" name="srn" value="'.$id.'" id="srn"  ><br>';
                     
-                    ?>
-                    
-                   
-                </tbody>
-            </table>
-
-
-
-        </div>
+                echo '</div>';
+                echo '<h1>Category Name</h1>';
+                echo '<div class="input-box">';
+                    echo '<input type="text" name="Name" value="'.$row['Name'].'" id="name" onblur="validatename()"  placeholder="Category name"><br>';
+                    echo '<span id="err1"></span>';
+                echo '</div>';
+                echo '<br><hr>'
+                ?>
+                <button type="submit" name="submit" class="save-btn">Save</button>
+    
+            <?php
+            echo '</div>';
+        echo '</form>';
+        ?>
         
-
-
         <!--
         <footer>
-            <div class="main-content">
+            <div class="main-content">>
                 <div class="left box">
                     <h2>About Us</h2>
                     <div class="content">
@@ -198,14 +141,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
             </div>
         </footer>-->
     </div>
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
-        crossorigin="anonymous"></script>
-
-    
 </body>
-
 </html>
+
